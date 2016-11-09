32b93f212cd1fbcbb6431cfa51917fcdbe6f4883
mary-13 <marinakubrina@mail.ru>
2016-11-09 15:44:31 +0600

    Create new groups

--- Alchemy/js/alchemy.js
+++ Alchemy/js/alchemy.js
@@ -12,8 +12,11 @@ window.onload = function alchemy(){
 var elements = [
     {name: "water", color: "#87CEFA"}, 
     {name: "fire", color: " #FF4500"},
-    {name: "air", color: "#F8F8FF"}, 
-    {name: "earth", color: "#CD853F"}];
+    {name: "air", color: "#F8F8FF"},
+    {name: "earth", color: "#CD853F"},
+    {name: "energy", color: "#A9A9A9"}, 
+    {name: "bacteries", color: " #DDA0DD"},
+    {name: "animals", color: "#32CD32"}];
 
 var svg = d3.select("body")
     .append("svg")
@@ -26,62 +29,78 @@ svg.append("rect")
     .attr("height", "100%")
     .attr("fill", "#000000");
     
-svg.append("g").selectAll("rect")
+svg.append("g")
+        .attr("class", "first-elements-container")
+        .selectAll("rect")
         .data(elements)
         .enter()
         .append("rect")
-        .attr("width", 60)
-        .attr("height", 60)
+        .attr("width", 45)
+        .attr("height", 45)
         .attr("x", offsetX)
-        .attr("y", function(d, i){ return i*70 + offsetY;})
-        .attr("fill", function(d){ return d.color;})
+        .attr("y", function(d, i) {return i*55 + offsetY})
+        .attr("fill", function(d){return d.color;})
         .on("click", onElementClick);
 
-svg.append("g").selectAll("rect")
+svg.append("g")
+        .attr("class", "second-elements-container")
+        .selectAll("rect")
         .data(elements)
         .enter()
         .append("rect")
-        .attr("width", 60)
-        .attr("height", 60)
+        .attr("width", 45)
+        .attr("height", 45)
         .attr("x", 780 + offsetX)
-        .attr("y", function(d, i){ return i*70 + offsetY;})
+        .attr("y", function(d, i) {return i*55 + offsetY})
         .attr("fill", function(d){ return d.color;})
         .on("click", onRightElementClick);
 
+//игровое поле
 svg       
         .append("g")
         .append("rect")
-        .attr("width", 700)
-        .attr("height", 300)
-        .attr("x", 70 + offsetX)
+        .attr("width", 715)
+        .attr("height", 375)
+        .attr("x", 55 + offsetX)
+        .attr("y", offsetY)
         .attr("fill", "#FFFFE0");
 
+//последнее действие
 svg       
         .append("g")
         .append("rect")
-        .attr("width", 700)
+        .attr("width", 715)
         .attr("height", 50)
-        .attr("x", 70 + offsetX)
-        .attr("y", 310 + offsetY)
+        .attr("x", 55 + offsetX)
+        .attr("y", 385 + offsetY)
         .attr("fill", "#DAA520");
 
+//elements on the left
 function onElementClick(d) {
     svg
             .append("rect")
             .attr("width", 60)
             .attr("height", 60)
             .attr("x", 100 + offsetX)
-            .attr("y", 110 + offsetY)
-            .attr("fill", function(){ return d.color;})
+            .attr("y", 160 + offsetY)
+            .attr("fill", function(){ return d.color;});
+            svg.append("g")
+            .append("rect")
+            .attr("width", 305)
+            .attr("height", 55)
+            .attr("x", 100 + offsetX)
+            .attr("y", 50 + offsetY)
+            .attr("fill", "#000000");
 }
 
+//elements on the right
 function onRightElementClick(d) {
     svg
             .append("rect")
             .attr("width", 60)
             .attr("height", 60)
-            .attr("x", 680 + offsetX)
-            .attr("y", 110 + offsetY)
+            .attr("x", 665 + offsetX)
+            .attr("y", 160 + offsetY)
             .attr("fill", function(){ return d.color;})
 }
 
